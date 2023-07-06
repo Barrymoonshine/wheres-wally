b545ee24469a790564508482d3b711998cdd125e
Nick Sheward <nickshwrd@gmail.com>
2023-07-06 15:59:19 +0100

    Firebase installed and config file created in utils directory

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWfynwwXOweY2U5PpxKzJBpfjEFHR6Lc4',
+  authDomain: 'where-s-wally-9cac8.firebaseapp.com',
+  projectId: 'where-s-wally-9cac8',
+  storageBucket: 'where-s-wally-9cac8.appspot.com',
+  messagingSenderId: '567268274327',
+  appId: '1:567268274327:web:8fef1f246bd6cf719939d2',
+};
+
+export default firebaseConfig;
