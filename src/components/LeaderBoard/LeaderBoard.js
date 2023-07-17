add55e47f7dcfd0a37ba9f25b163a53a5ebb9f01
Nick Sheward <nickshwrd@gmail.com>
2023-07-17 09:00:52 +0100

    Table styling applied to LeaderBoard component

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,6 +1,7 @@
 import './LeaderBoard.css';
 import useGameState from '../../hooks/useGameState';
 import useGameDispatch from '../../hooks/useGameDispatch';
+import { getSeconds, getMinutes } from '../../helpers/helpers';
 
 const LeaderBoard = () => {
   const { leaderBoard } = useGameState();
@@ -10,14 +11,20 @@ const LeaderBoard = () => {
     <div className='leader-board-modal'>
       <div className='leader-board-modal-content'>
         <div className='modal-header'>Leader board</div>
-        Dynamically populate list with top ten from Firebase
-        <ul>
-          {leaderBoard.map((player) => (
-            <li key={player.id}>
-              Name: {player.name} Time : {player.seconds}
-            </li>
-          ))}
-        </ul>
+        <div className='leader-board-first-row'>
+          <div className='leader-board-columns'>Position</div>
+          <div className='leader-board-columns'>Name</div>
+          <div className='leader-board-columns'>Time</div>
+        </div>
+        {leaderBoard.map((player, index) => (
+          <div className='leader-board-dynamic-rows' key={player.id}>
+            <div className='leader-board-columns'>{index}</div>
+            <div className='leader-board-columns'>{player.name}</div>
+            <div className='leader-board-columns'>
+              {getMinutes(player.seconds)}:{getSeconds(player.seconds)}
+            </div>
+          </div>
+        ))}
         <button className='play-again-button' onClick={() => playAgain()}>
           Play again?
         </button>
